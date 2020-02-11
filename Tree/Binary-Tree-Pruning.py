8286b7bdee7b9280c28847a9e38777dcc0a93a7d
Ajinkya Patankar <ajinkya.patankar@gmail.com>
2020-02-10 20:03:27 -0500

    Added Binary Tree Pruning solution
    Added Binary Tree Pruning solution

---
+++ Tree/Binary-Tree-Pruning.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def pruneTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        def containsOne(node):
+            if not node:
+                return False
+            a1 = containsOne(node.left)
+            a2 = containsOne(node.right)
+            if not a1: node.left = None
+            if not a2: node.right = None
+            
+            return node.val == 1 or a1 or a2
+            
+        return root if containsOne(root) else None
+        
