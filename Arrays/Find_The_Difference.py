b58278015a1b0ba6f003149066f099289f872c82
Ajinkya Patankar <ajinkya.patankar@gmail.com>
2020-02-18 19:39:36 -0500

    Added find the differnece solution
    Added find the differnece solution

---
+++ Arrays/Find_The_Difference.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def findTheDifference(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: str
+        """
+        s = list(s)
+        t = list(t)
+        
+        for ch in s:
+            t.remove(ch)
+            
+        return t[0]
